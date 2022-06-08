130c1e8eac00c48ab9c4753e103b0c070996e835
xzhw39 <xzhw39@gmail.com>
2022-06-07 21:20:15 -0400

    modified background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,34 @@
 var today = moment().format("dddd, MMMM Do, YYYY");
 $("#currentDay").text(today);
 
+// Declare new variables
+
+var divEl = $('<div>');
+
+var inputEl = $('<input>');
+
+inputEl.attr('type', 'text');
+
+
+// Append new elements
+
+divEl.append(inputEl);
+
+// Element attributes
+
+// inputEl.attr("type")
+
+$('.timeRow').css({'background-color': 'white'});
+
+$(':text').css('background-color', 'blue');
+
+//click event and local storage
+$("button").on("click", function (event) {
+    event.preventDefault();
+
+    var textInput = $(this).siblings("input").attr("data");
+    var textVal = $(this).siblings("input").val();
+    localStorage.setItem(textInput, textVal);
+    var lsVal = localStorage.getItem(textInput);
+    $(this).siblings("input").text(lsVal);
+});
